d311d6df50a33e8d5ccf12c95923aceb1ccfa980
João Antônio Cardoso <joao.maker@gmail.com>
2024-09-07 14:51:18 -0300

    benches: Add callbacks benchmark

---
+++ benches/callbacks_bench.rs
@@ -0,0 +1,48 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use mavlink_server::callbacks::Callbacks;
+use tokio::runtime::Runtime;
+
+fn bench_call_all(c: &mut Criterion) {
+    let mut group = c.benchmark_group("callbacks");
+
+    let callback_counts = vec![0, 1, 3, 5, 10, 20, 50, 100];
+
+    for number_of_callbacks in &callback_counts {
+        group.throughput(criterion::Throughput::Elements(*number_of_callbacks));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(number_of_callbacks),
+            number_of_callbacks,
+            |b, &number_of_callbacks| {
+                let rt = Runtime::new().unwrap();
+                let callbacks = Callbacks::<String>::new();
+
+                for _ in 0..number_of_callbacks {
+                    callbacks.add_callback({
+                        move |msg: String| async move {
+                            if msg != "test" {
+                                panic!("Wrong message");
+                            }
+                            Ok(())
+                        }
+                    });
+                }
+
+                // Benchmark calling all callbacks
+                b.iter(|| {
+                    rt.block_on(async {
+                        for future in callbacks.call_all("test".to_string()) {
+                            if let Err(_error) = future.await {
+                                continue;
+                            }
+                        }
+                    });
+                });
+            },
+        );
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_call_all);
+criterion_main!(benches);
