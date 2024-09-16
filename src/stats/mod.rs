1e8f8253a226e57817bd71571aacda1dc1bcf881
João Antônio Cardoso <joao.maker@gmail.com>
2024-09-16 16:30:35 -0300

    src: stats: Add hub stats pub interface

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -60,6 +60,16 @@ impl Stats {
         response_rx.await?
     }
 
+    pub async fn hub_stats(&mut self) -> Result<StatsInner> {
+        let (response_tx, response_rx) = oneshot::channel();
+        self.sender
+            .send(StatsCommand::GetHubStats {
+                response: response_tx,
+            })
+            .await?;
+        response_rx.await?
+    }
+
     pub async fn set_period(&mut self, period: tokio::time::Duration) -> Result<()> {
         let (response_tx, response_rx) = oneshot::channel();
         self.sender
