2a0e166a5b037c5802949fff72dcd62ab4d4a973
Patrick José Pereira <patrickelectric@gmail.com>
2024-09-19 16:29:27 -0300

    stats: accumulated: Add last mavlink message
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/stats/accumulated/mod.rs
+++ src/stats/accumulated/mod.rs
@@ -7,6 +7,7 @@ use crate::protocol::Protocol;
 
 #[derive(Clone, Debug)]
 pub struct AccumulatedStatsInner {
+    pub last_message: Option<Arc<Protocol>>,
     pub last_update_us: u64,
     pub messages: u64,
     pub bytes: u64,
@@ -16,6 +17,7 @@ pub struct AccumulatedStatsInner {
 impl Default for AccumulatedStatsInner {
     fn default() -> Self {
         Self {
+            last_message: None,
             last_update_us: chrono::Utc::now().timestamp_micros() as u64,
             messages: 0,
             bytes: 0,
@@ -26,6 +28,7 @@ impl Default for AccumulatedStatsInner {
 
 impl AccumulatedStatsInner {
     pub fn update(&mut self, message: &Arc<Protocol>) {
+        self.last_message = Some(message.clone());
         self.last_update_us = chrono::Utc::now().timestamp_micros() as u64;
         self.bytes = self.bytes.wrapping_add(message.raw_bytes().len() as u64);
         self.messages = self.messages.wrapping_add(1);
