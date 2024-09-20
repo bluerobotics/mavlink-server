68d26aa9ff5c143ec1551a3b61717b2125d1e49d
João Antônio Cardoso <joao.maker@gmail.com>
2024-09-20 00:08:01 -0300

    src: stats: accumulated: Add driver name and type to AccumulatedDriverStats

--- src/stats/accumulated/driver.rs
+++ src/stats/accumulated/driver.rs
@@ -1,8 +1,12 @@
 use std::sync::Arc;
 
-use crate::protocol::Protocol;
+use indexmap::IndexMap;
+
+use crate::{drivers::DriverInfo, protocol::Protocol, stats::driver::DriverUuid};
 
 use super::AccumulatedStatsInner;
+
+pub type AccumulatedDriversStats = IndexMap<DriverUuid, AccumulatedDriverStats>;
 
 #[async_trait::async_trait]
 pub trait AccumulatedDriverStatsProvider {
@@ -10,13 +14,30 @@ pub trait AccumulatedDriverStatsProvider {
     async fn reset_stats(&self);
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct AccumulatedDriverStats {
+    pub name: Arc<String>,
+    pub driver_type: &'static str,
+    pub stats: AccumulatedDriverStatsInner,
+}
+
+impl AccumulatedDriverStats {
+    pub fn new(name: Arc<String>, info: &dyn DriverInfo) -> Self {
+        Self {
+            name,
+            driver_type: info.name(),
+            stats: AccumulatedDriverStatsInner::default(),
+        }
+    }
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct AccumulatedDriverStatsInner {
     pub input: Option<AccumulatedStatsInner>,
     pub output: Option<AccumulatedStatsInner>,
 }
 
-impl AccumulatedDriverStats {
+impl AccumulatedDriverStatsInner {
     pub fn update_input(&mut self, message: &Arc<Protocol>) {
         if let Some(stats) = self.input.as_mut() {
             stats.update(message);
