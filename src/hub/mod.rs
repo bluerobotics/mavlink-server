d27b105b0cf631626a29296a7fb0a8444a100b41
Patrick José Pereira <patrickelectric@gmail.com>
2024-10-02 13:08:22 -0300

    hub: Move to singleton pattern
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>
    
    global hub
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/hub/mod.rs
+++ src/hub/mod.rs
@@ -4,7 +4,9 @@ mod protocol;
 use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
+use async_once::AsyncOnce;
 use indexmap::IndexMap;
+use lazy_static::lazy_static;
 use tokio::sync::{broadcast, mpsc, oneshot, RwLock};
 
 use crate::{
@@ -22,8 +24,21 @@ use crate::{
 use actor::HubActor;
 use protocol::HubCommand;
 
-#[derive(Clone)]
-pub struct Hub {
+lazy_static! {
+    static ref HUB: AsyncOnce<Hub> = AsyncOnce::new(async {
+        Hub::new(
+            10000,
+            Arc::new(RwLock::new(
+                mavlink::ardupilotmega::MavComponent::MAV_COMP_ID_ONBOARD_COMPUTER as u8,
+            )),
+            Arc::new(RwLock::new(1)),
+            Arc::new(RwLock::new(1.)),
+        )
+        .await
+    });
+}
+
+struct Hub {
     sender: mpsc::Sender<HubCommand>,
     _task: Arc<Mutex<tokio::task::JoinHandle<()>>>,
 }
@@ -40,91 +55,107 @@ impl Hub {
         let _task = Arc::new(Mutex::new(tokio::spawn(hub.start(receiver))));
         Self { sender, _task }
     }
+}
 
-    pub async fn add_driver(&self, driver: Arc<dyn Driver>) -> Result<DriverUuid> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(HubCommand::AddDriver {
-                driver,
-                response: response_tx,
-            })
-            .await?;
-        response_rx.await?
-    }
+pub async fn add_driver(driver: Arc<dyn Driver>) -> Result<DriverUuid> {
+    let (response_tx, response_rx) = oneshot::channel();
+    HUB.get()
+        .await
+        .sender
+        .send(HubCommand::AddDriver {
+            driver,
+            response: response_tx,
+        })
+        .await?;
+    response_rx.await?
+}
 
-    pub async fn remove_driver(&self, uuid: DriverUuid) -> Result<()> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(HubCommand::RemoveDriver {
-                uuid,
-                response: response_tx,
-            })
-            .await?;
-        response_rx.await?
-    }
+pub async fn remove_driver(uuid: DriverUuid) -> Result<()> {
+    let (response_tx, response_rx) = oneshot::channel();
+    HUB.get()
+        .await
+        .sender
+        .send(HubCommand::RemoveDriver {
+            uuid,
+            response: response_tx,
+        })
+        .await?;
+    response_rx.await?
+}
 
-    pub async fn drivers(&self) -> Result<IndexMap<DriverUuid, Box<dyn DriverInfo>>> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(HubCommand::GetDrivers {
-                response: response_tx,
-            })
-            .await?;
-        let res = response_rx.await?;
-        Ok(res)
-    }
+pub async fn drivers() -> Result<IndexMap<DriverUuid, Box<dyn DriverInfo>>> {
+    let (response_tx, response_rx) = oneshot::channel();
+    HUB.get()
+        .await
+        .sender
+        .send(HubCommand::GetDrivers {
+            response: response_tx,
+        })
+        .await?;
+    let res = response_rx.await?;
+    Ok(res)
+}
 
-    pub async fn sender(&self) -> Result<broadcast::Sender<Arc<Protocol>>> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(HubCommand::GetSender {
-                response: response_tx,
-            })
-            .await?;
-        let res = response_rx.await?;
-        Ok(res)
-    }
+pub async fn sender() -> Result<broadcast::Sender<Arc<Protocol>>> {
+    let (response_tx, response_rx) = oneshot::channel();
+    HUB.get()
+        .await
+        .sender
+        .send(HubCommand::GetSender {
+            response: response_tx,
+        })
+        .await?;
+    let res = response_rx.await?;
+    Ok(res)
+}
 
-    pub async fn drivers_stats(&self) -> Result<AccumulatedDriversStats> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(HubCommand::GetDriversStats {
-                response: response_tx,
-            })
-            .await?;
-        let res = response_rx.await?;
-        Ok(res)
-    }
+pub async fn drivers_stats() -> Result<AccumulatedDriversStats> {
+    let (response_tx, response_rx) = oneshot::channel();
+    HUB.get()
+        .await
+        .sender
+        .send(HubCommand::GetDriversStats {
+            response: response_tx,
+        })
+        .await?;
+    let res = response_rx.await?;
+    Ok(res)
+}
 
-    pub async fn hub_stats(&self) -> Result<AccumulatedStatsInner> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(HubCommand::GetHubStats {
-                response: response_tx,
-            })
-            .await?;
-        let res = response_rx.await?;
-        Ok(res)
-    }
+pub async fn hub_stats() -> Result<AccumulatedStatsInner> {
+    let (response_tx, response_rx) = oneshot::channel();
+    HUB.get()
+        .await
+        .sender
+        .send(HubCommand::GetHubStats {
+            response: response_tx,
+        })
+        .await?;
+    let res = response_rx.await?;
+    Ok(res)
+}
 
-    pub async fn hub_messages_stats(&self) -> Result<AccumulatedHubMessagesStats> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(HubCommand::GetHubMessagesStats {
-                response: response_tx,
-            })
-            .await?;
-        let res = response_rx.await?;
-        Ok(res)
-    }
+pub async fn hub_messages_stats() -> Result<AccumulatedHubMessagesStats> {
+    let (response_tx, response_rx) = oneshot::channel();
+    HUB.get()
+        .await
+        .sender
+        .send(HubCommand::GetHubMessagesStats {
+            response: response_tx,
+        })
+        .await?;
+    let res = response_rx.await?;
+    Ok(res)
+}
 
-    pub async fn reset_all_stats(&self) -> Result<()> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(HubCommand::ResetAllStats {
-                response: response_tx,
-            })
-            .await?;
-        response_rx.await?
-    }
+pub async fn reset_all_stats() -> Result<()> {
+    let (response_tx, response_rx) = oneshot::channel();
+    HUB.get()
+        .await
+        .sender
+        .send(HubCommand::ResetAllStats {
+            response: response_tx,
+        })
+        .await?;
+    response_rx.await?
 }
