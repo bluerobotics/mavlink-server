53a8db387c8f131be79d9ab20dc35b2629fab272
Patrick José Pereira <patrickelectric@gmail.com>
2024-10-03 01:14:46 -0300

    webpage: app: format file
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/webpage/src/app.rs
+++ src/webpage/src/app.rs
@@ -45,9 +45,7 @@ impl App {
         let url = format!("{}//{}/rest/ws", protocol, host);
 
         let (sender, receiver) = {
-            let url = Url::parse(&url)
-                .unwrap()
-                .to_string();
+            let url = Url::parse(&url).unwrap().to_string();
             connect(url, ewebsock::Options::default()).expect("Can't connect")
         };
 
@@ -68,81 +66,83 @@ impl App {
                 ui.label("MAVLink Server");
                 ui.add_space(16.0);
 
-                ui.with_layout(eframe::egui::Layout::right_to_left(eframe::egui::Align::RIGHT), |ui| {
-                    eframe::egui::widgets::global_dark_light_mode_switch(ui);
-                    ui.separator();
-                });
+                ui.with_layout(
+                    eframe::egui::Layout::right_to_left(eframe::egui::Align::RIGHT),
+                    |ui| {
+                        eframe::egui::widgets::global_dark_light_mode_switch(ui);
+                        ui.separator();
+                    },
+                );
             });
         });
     }
 
-
-fn process_mavlink_websocket(&mut self) {
-    while let Some(ewebsock::WsEvent::Message(ewebsock::WsMessage::Text(message))) = self.receiver.try_recv() {
-        let Ok(message_json) = serde_json::from_str::<serde_json::Value>(&message)
-        else {
-            continue;
-        };
-        let Some(system_id) = message_json["header"]["system_id"]
-            .as_u64()
-            .map(|n| n as u8)
-        else {
-            continue;
-        };
-        let Some(component_id) = message_json["header"]["component_id"]
-            .as_u64()
-            .map(|n| n as u8)
-        else {
-            continue;
-        };
-        let Some(message_name) = message_json["message"]["type"]
-            .as_str()
-            .map(|s| s.to_string())
-        else {
-            continue;
-        };
-        self.vehicles
-            .entry(system_id)
-            .or_default()
-            .entry(component_id)
-            .or_default();
-        let Some(vehicle) = self.vehicles.get_mut(&system_id) else {
-            continue;
-        };
-        let Some(messages) = vehicle.get_mut(&component_id) else {
-            continue;
-        };
-        let message_info =
-            messages.entry(message_name).or_insert_with(|| MessageInfo {
+    fn process_mavlink_websocket(&mut self) {
+        while let Some(ewebsock::WsEvent::Message(ewebsock::WsMessage::Text(message))) =
+            self.receiver.try_recv()
+        {
+            let Ok(message_json) = serde_json::from_str::<serde_json::Value>(&message) else {
+                continue;
+            };
+            let Some(system_id) = message_json["header"]["system_id"]
+                .as_u64()
+                .map(|n| n as u8)
+            else {
+                continue;
+            };
+            let Some(component_id) = message_json["header"]["component_id"]
+                .as_u64()
+                .map(|n| n as u8)
+            else {
+                continue;
+            };
+            let Some(message_name) = message_json["message"]["type"]
+                .as_str()
+                .map(|s| s.to_string())
+            else {
+                continue;
+            };
+            self.vehicles
+                .entry(system_id)
+                .or_default()
+                .entry(component_id)
+                .or_default();
+            let Some(vehicle) = self.vehicles.get_mut(&system_id) else {
+                continue;
+            };
+            let Some(messages) = vehicle.get_mut(&component_id) else {
+                continue;
+            };
+            let message_info = messages.entry(message_name).or_insert_with(|| MessageInfo {
                 last_sample_time: self.now,
                 fields: Default::default(),
             });
-        message_info.last_sample_time = self.now;
-        let Some(fields) = message_json["message"].as_object() else {
-            continue;
-        };
-        for (field_name, value) in fields {
-            if field_name == "type" {
-                continue;
-            }
-            let Some(num) = extract_number(value) else {
+            message_info.last_sample_time = self.now;
+            let Some(fields) = message_json["message"].as_object() else {
                 continue;
             };
-            let field_info = message_info
-                .fields
-                .entry(field_name.clone())
-                .or_insert_with(|| FieldInfo {
-                    history: Vec::new(),
-                    latest_value: num,
-                });
-            field_info.latest_value = num;
-            field_info.history.push((self.now, num));
-            if field_info.history.len() > 1000 {
-                field_info.history.remove(0);
+            for (field_name, value) in fields {
+                if field_name == "type" {
+                    continue;
+                }
+                let Some(num) = extract_number(value) else {
+                    continue;
+                };
+                let field_info = message_info
+                    .fields
+                    .entry(field_name.clone())
+                    .or_insert_with(|| FieldInfo {
+                        history: Vec::new(),
+                        latest_value: num,
+                    });
+                field_info.latest_value = num;
+                field_info.history.push((self.now, num));
+                if field_info.history.len() > 1000 {
+                    field_info.history.remove(0);
+                }
             }
         }
     }
-}
 
     fn create_messages_ui(&mut self, ui: &mut eframe::egui::Ui) {
         let search_query = self.search_query.to_lowercase();
@@ -155,21 +155,19 @@ fn process_mavlink_websocket(&mut self) {
 
                     for (name, message) in messages {
                         let name_lower = name.to_lowercase();
-                        let message_matches = search_query.is_empty()
-                            || name_lower.contains(&search_query);
+                        let message_matches =
+                            search_query.is_empty() || name_lower.contains(&search_query);
 
                         let mut matching_fields = BTreeMap::new();
 
                         if message_matches {
-                            matching_fields.extend(
-                                message.fields.iter().map(|(k, v)| (k.clone(), v.clone())),
-                            );
+                            matching_fields
+                                .extend(message.fields.iter().map(|(k, v)| (k.clone(), v.clone())));
                         } else {
                             for (field_name, field_info) in &message.fields {
                                 let field_name_lower = field_name.to_lowercase();
                                 if field_name_lower.contains(&search_query) {
-                                    matching_fields
-                                        .insert(field_name.clone(), field_info.clone());
+                                    matching_fields.insert(field_name.clone(), field_info.clone());
                                 }
                             }
                         }
@@ -195,26 +193,22 @@ fn process_mavlink_websocket(&mut self) {
 
                     vehicle_header.show(ui, |ui| {
                         for (component_id, messages) in matching_components {
-                            let component_id_str = format!("component_{}_{}", system_id, component_id);
+                            let component_id_str =
+                                format!("component_{}_{}", system_id, component_id);
                             let component_id_hash = ui.make_persistent_id(&component_id_str);
-                            let component_header = CollapsingHeader::new(format!(
-                                "Component {component_id}"
-                            ))
-                            .id_salt(component_id_hash)
-                            .default_open(true);
+                            let component_header =
+                                CollapsingHeader::new(format!("Component {component_id}"))
+                                    .id_salt(component_id_hash)
+                                    .default_open(true);
 
                             component_header.show(ui, |ui| {
-                                for (
-                                    name,
-                                    (message, matching_fields, _message_matches),
-                                ) in messages
+                                for (name, (message, matching_fields, _message_matches)) in messages
                                 {
                                     let message_id_str =
                                         format!("message_{}_{}_{}", system_id, component_id, name);
-                                    let message_id_hash =
-                                        ui.make_persistent_id(&message_id_str);
-                                    let mut message_header = CollapsingHeader::new(name)
-                                        .id_salt(message_id_hash);
+                                    let message_id_hash = ui.make_persistent_id(&message_id_str);
+                                    let mut message_header =
+                                        CollapsingHeader::new(name).id_salt(message_id_hash);
 
                                     if self.expand_all {
                                         message_header = message_header.open(Some(true));
@@ -242,9 +236,8 @@ fn process_mavlink_websocket(&mut self) {
                                                             .history
                                                             .iter()
                                                             .map(|(time, value)| {
-                                                                let timestamp = time
-                                                                    .timestamp_millis()
-                                                                    as f64;
+                                                                let timestamp =
+                                                                    time.timestamp_millis() as f64;
                                                                 [timestamp, *value]
                                                             })
                                                             .collect();
@@ -255,9 +248,10 @@ fn process_mavlink_websocket(&mut self) {
                                                         Plot::new(field_name.clone())
                                                             .view_aspect(2.0)
                                                             .x_axis_formatter(|x, _range| {
-                                                                let datetime = DateTime::from_timestamp_millis(
-                                                                    x.value as i64,
-                                                                );
+                                                                let datetime =
+                                                                    DateTime::from_timestamp_millis(
+                                                                        x.value as i64,
+                                                                    );
                                                                 if let Some(dt) = datetime {
                                                                     dt.format("%H:%M:%S")
                                                                         .to_string()
@@ -266,9 +260,10 @@ fn process_mavlink_websocket(&mut self) {
                                                                 }
                                                             })
                                                             .label_formatter(|name, value| {
-                                                                let datetime = DateTime::from_timestamp_millis(
-                                                                    value.x as i64,
-                                                                );
+                                                                let datetime =
+                                                                    DateTime::from_timestamp_millis(
+                                                                        value.x as i64,
+                                                                    );
                                                                 if let Some(dt) = datetime {
                                                                     format!(
                                                                         "{}: {:.2}\nTime: {}",
