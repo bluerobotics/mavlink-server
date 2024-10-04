ee4311ecfdcacac8a7675a36663673d8ad15a171
João Antônio Cardoso <joao.maker@gmail.com>
2024-10-04 00:46:35 -0300

    src: stats: Implement Serializer for stats data

--- src/stats/driver.rs
+++ src/stats/driver.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use indexmap::IndexMap;
+use serde::Serialize;
 
 use super::StatsInner;
 
@@ -8,14 +9,14 @@ pub type DriverUuid = uuid::Uuid;
 
 pub type DriversStats = IndexMap<DriverUuid, DriverStats>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct DriverStats {
     pub name: Arc<String>,
     pub driver_type: &'static str,
     pub stats: DriverStatsInner,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct DriverStatsInner {
     pub input: Option<StatsInner>,
     pub output: Option<StatsInner>,
--- src/stats/messages.rs
+++ src/stats/messages.rs
@@ -1,4 +1,5 @@
 use indexmap::IndexMap;
+use serde::Serialize;
 
 use super::StatsInner;
 
@@ -6,17 +7,17 @@ pub type SystemId = u8;
 pub type ComponentId = u8;
 pub type MessageId = u32;
 
-#[derive(Default, Clone, Debug)]
+#[derive(Default, Clone, Debug, Serialize)]
 pub struct HubMessagesStats {
     pub systems_messages_stats: IndexMap<SystemId, SystemMessagesStats>,
 }
 
-#[derive(Default, Clone, Debug)]
+#[derive(Default, Clone, Debug, Serialize)]
 pub struct SystemMessagesStats {
     pub components_messages_stats: IndexMap<ComponentId, ComponentMessageStats>,
 }
 
-#[derive(Default, Clone, Debug)]
+#[derive(Default, Clone, Debug, Serialize)]
 pub struct ComponentMessageStats {
     pub messages_stats: IndexMap<MessageId, StatsInner>,
 }
