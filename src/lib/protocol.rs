2915fef9d8056a6d4fd326541ae4d6c6f7cf20cd
João Antônio Cardoso <joao.maker@gmail.com>
2024-10-21 21:13:16 -0300

    src: lib: protocol: Remove unused function and imports

--- src/lib/protocol.rs
+++ src/lib/protocol.rs
@@ -1,13 +1,7 @@
-use std::{
-    future::Future,
-    io::Cursor,
-    ops::{Deref, DerefMut},
-};
+use std::ops::{Deref, DerefMut};
 
 use mavlink_codec::Packet;
 use serde::Serialize;
-
-use tracing::*;
 
 #[derive(Debug, PartialEq, Serialize)]
 pub struct Protocol {
@@ -33,39 +27,6 @@ impl Protocol {
             packet,
         }
     }
-}
-
-pub async fn read_all_messages<F, Fut>(origin: &str, buf: &mut Vec<u8>, process_message: F)
-where
-    F: Fn(Protocol) -> Fut,
-    Fut: Future<Output = ()>,
-{
-    let reader = Cursor::new(buf.as_slice());
-    let mut reader: mavlink::async_peek_reader::AsyncPeekReader<Cursor<&[u8]>, 280> =
-        mavlink::async_peek_reader::AsyncPeekReader::new(reader);
-
-    loop {
-        let message = match mavlink::read_v2_raw_message_async::<MavMessage, _>(&mut reader).await {
-            Ok(message) => Protocol::new(origin, message),
-            Err(error) => {
-                match error {
-                    mavlink::error::MessageReadError::Io(_) => (),
-                    mavlink::error::MessageReadError::Parse(_) => {
-                        error!("Failed to parse MAVLink message: {error:?}")
-                    }
-                }
-
-                break;
-            }
-        };
-
-        trace!("Parsed message: {:?}", message.raw_bytes());
-
-        process_message(message).await;
-    }
-
-    let bytes_read = reader.reader_ref().position() as usize;
-    buf.drain(..bytes_read);
 }
 
 impl Deref for Protocol {
