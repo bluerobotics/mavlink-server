d23f8a31609e3708f95676883dbf18893d79b2f4
João Antônio Cardoso <joao.maker@gmail.com>
2024-11-13 01:00:00 -0300

    src: lib: Implement Protocol conversion methods using the configured mavlink_version

--- src/lib/protocol.rs
+++ src/lib/protocol.rs
@@ -1,7 +1,13 @@
 use std::ops::{Deref, DerefMut};
 
+use anyhow::Result;
 use mavlink_codec::Packet;
 use serde::Serialize;
+
+use crate::{
+    cli,
+    mavlink_json::{MAVLinkJSON, MAVLinkJSONHeader},
+};
 
 #[derive(Debug, PartialEq, Serialize)]
 pub struct Protocol {
@@ -26,6 +32,52 @@ impl Protocol {
             timestamp,
             packet,
         }
+    }
+
+    pub fn from_mavlink_raw<M>(header: mavlink::MavHeader, message: &M, origin: &str) -> Self
+    where
+        M: mavlink::Message,
+    {
+        let packet = match cli::mavlink_version() {
+            1 => {
+                let mut message_raw = mavlink::MAVLinkV1MessageRaw::new();
+                message_raw.serialize_message(header, message);
+                Packet::from(message_raw)
+            }
+            2 => {
+                let mut message_raw = mavlink::MAVLinkV2MessageRaw::new();
+                message_raw.serialize_message(header, message);
+                Packet::from(message_raw)
+            }
+            _ => unreachable!(),
+        };
+
+        Self {
+            origin: origin.to_string(),
+            timestamp: chrono::Utc::now().timestamp_micros() as u64,
+            packet,
+        }
+    }
+
+    pub async fn to_mavlink_json<M>(&self) -> Result<MAVLinkJSON<M>>
+    where
+        M: mavlink::Message,
+    {
+        let mut reader = mavlink::async_peek_reader::AsyncPeekReader::new(self.as_slice());
+
+        let (header, message) = match cli::mavlink_version() {
+            1 => mavlink::read_v1_msg_async::<M, _>(&mut reader).await,
+            2 => mavlink::read_v2_msg_async::<M, _>(&mut reader).await,
+            _ => unreachable!(),
+        }
+        .map_err(anyhow::Error::msg)?;
+
+        let header = MAVLinkJSONHeader {
+            inner: header,
+            message_id: Some(mavlink::Message::message_id(&message)),
+        };
+
+        Ok(MAVLinkJSON { header, message })
     }
 }
 
