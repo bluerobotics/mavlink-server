83b214e853a8969354e26797e99ae4d7a3732779
João Antônio Cardoso <joao.maker@gmail.com>
2024-11-13 22:21:42 -0300

    src: lib: web: Move and split log endpoint into routes/v1/log

---
+++ src/lib/web/routes/v1/log.rs
@@ -0,0 +1,38 @@
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        WebSocketUpgrade,
+    },
+    response::Response,
+    routing::get,
+    Router,
+};
+use futures::{SinkExt, StreamExt};
+use tracing::*;
+
+#[instrument(level = "trace")]
+pub fn router() -> Router {
+    Router::new().route("/", get(log_websocket_handler))
+}
+
+async fn log_websocket_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(log_websocket_connection)
+}
+
+#[instrument(level = "debug", skip_all)]
+async fn log_websocket_connection(socket: WebSocket) {
+    let (mut websocket_sender, mut _websocket_receiver) = socket.split();
+    let (mut receiver, history) = crate::logger::HISTORY.lock().unwrap().subscribe();
+
+    for message in history {
+        if websocket_sender.send(Message::Text(message)).await.is_err() {
+            return;
+        }
+    }
+
+    while let Ok(message) = receiver.recv().await {
+        if websocket_sender.send(Message::Text(message)).await.is_err() {
+            break;
+        }
+    }
+}
