0bb6218956288efd1d6280e8c28c44a4653a9d47
João Antônio Cardoso <joao.maker@gmail.com>
2024-11-13 22:26:34 -0300

    src: lib: web: Move and split stats endpoints into routes/v1/stats

--- src/lib/web/endpoints.rs
+++ src/lib/web/endpoints.rs
@@ -2,20 +2,11 @@ use axum::{
     extract::Path,
     http::{header, StatusCode},
     response::IntoResponse,
-    Json,
 };
 use include_dir::{include_dir, Dir};
 use mime_guess::from_path;
-use serde::{Deserialize, Serialize};
-
-use crate::stats;
 
 static HTML_DIST: Dir = include_dir!("src/webpage/dist");
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Frequency {
-    pub frequency: f32,
-}
 
 pub async fn root(filename: Option<Path<String>>) -> impl IntoResponse {
     let filename = filename
@@ -40,30 +31,4 @@ pub async fn root(filename: Option<Path<String>>) -> impl IntoResponse {
             ([(header::CONTENT_TYPE, mime_type.as_ref())], content).into_response()
         },
     )
-}
-
-pub async fn drivers_stats() -> impl IntoResponse {
-    Json(stats::drivers_stats().await.unwrap())
-}
-
-pub async fn hub_stats() -> impl IntoResponse {
-    Json(stats::hub_stats().await.unwrap())
-}
-
-pub async fn hub_messages_stats() -> impl IntoResponse {
-    Json(stats::hub_messages_stats().await.unwrap())
-}
-
-pub async fn stats_frequency() -> impl IntoResponse {
-    let frequency = 1. / stats::period().await.unwrap().as_secs_f32();
-    Json(Frequency { frequency })
-}
-
-pub async fn set_stats_frequency(
-    Json(Frequency { frequency }): Json<Frequency>,
-) -> impl IntoResponse {
-    let period = tokio::time::Duration::from_secs_f32(1. / frequency.clamp(0.1, 10.));
-
-    let frequency = 1. / stats::set_period(period).await.unwrap().as_secs_f32();
-    Json(Frequency { frequency })
 }
