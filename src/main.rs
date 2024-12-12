8f1ec0420dfcfade8f90c47fd6a3a6d0b34f621c
Patrick José Pereira <patrickelectric@gmail.com>
2024-12-12 11:35:05 -0300

    main: Fix use order
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use anyhow::*;
-use tracing::*;
-
 use mavlink_server::{cli, hub, logger, web};
+use tracing::*;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() -> Result<()> {
