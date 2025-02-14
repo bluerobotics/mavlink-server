c89016ac72a766927bcb53eee02265449fdcffe9
Patrick José Pereira <patrickelectric@gmail.com>
2025-02-14 07:17:44 -0300

    tests: udp: Add test for udpclient
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/udp.rs
+++ tests/udp.rs
@@ -75,3 +75,40 @@ async fn test_udpserver_send_only() -> Result<()> {
 
     Ok(())
 }
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 10)]
+#[ignore]
+async fn test_udpclient_send_only() -> Result<()> {
+    cli::init_with(cli::Args::parse_from(vec![
+        "", // For some unknown reason, the first argument is ignored
+        "udpclient://0.0.0.0:3333?direction=sender",
+        "udpserver://0.0.0.0:3333",
+        "--mavlink-heartbeat-frequency",
+        "10",
+    ]));
+
+    for driver in cli::endpoints() {
+        hub::add_driver(driver).await?;
+    }
+
+    stats::set_period(tokio::time::Duration::from_millis(100)).await?;
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    for (_uuid, driver) in stats::drivers_stats().await? {
+        if *driver.name == "UdpServer" {
+            assert!(driver.stats.output.is_some());
+            assert!(driver.stats.input.is_some());
+        }
+
+        if *driver.name == "UdpClient" {
+            assert!(driver.stats.output.is_some());
+            assert!(driver.stats.input.is_none());
+        }
+    }
+
+    for (id, driver_info) in hub::drivers().await? {
+        debug!("Removing driver id {id:?} ({driver_info:?})");
+        hub::remove_driver(id).await?;
+    }
+
+    Ok(())
+}
