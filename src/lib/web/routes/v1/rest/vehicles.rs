a07a90aa6b9cf85c83a0bbf9d07378f355f152c2
Patrick José Pereira <patrickelectric@gmail.com>
2025-02-24 14:45:22 -0300

    lib: web: routes: rest: vehicles: First version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/lib/web/routes/v1/rest/vehicles.rs
@@ -0,0 +1,91 @@
+use std::net::SocketAddr;
+
+use crate::drivers::rest::control;
+use axum::{
+    extract::{
+        ws::{self, WebSocket},
+        ConnectInfo, Json, WebSocketUpgrade,
+    },
+    http::header,
+    response::IntoResponse,
+    response::Response,
+};
+use futures::{SinkExt, StreamExt};
+use serde::Deserialize;
+use tracing::*;
+use uuid::Uuid;
+
+#[derive(Debug, Deserialize)]
+pub struct ArmPayload {
+    vehicle_id: Option<u8>,
+    component_id: Option<u8>,
+    force: Option<bool>,
+}
+
+pub(crate) async fn vehicles() -> impl IntoResponse {
+    let vehicles = control::vehicles().await;
+    let json = serde_json::to_string_pretty(&vehicles).unwrap();
+    ([(header::CONTENT_TYPE, "application/json")], json).into_response()
+}
+
+pub(crate) async fn arm(Json(payload): Json<ArmPayload>) -> impl IntoResponse {
+    debug!("Arming vehicle: {:#?}", &payload);
+    let _ = control::arm(payload.vehicle_id, payload.component_id, payload.force);
+}
+
+pub(crate) async fn disarm(Json(payload): Json<ArmPayload>) -> impl IntoResponse {
+    debug!("Disarming vehicle: {:#?}", &payload);
+    let _ = control::disarm(payload.vehicle_id, payload.component_id, payload.force);
+}
+
+#[instrument(level = "debug", skip_all)]
+pub(crate) async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> Response {
+    ws.on_upgrade(move |socket| websocket_connection(socket, addr))
+}
+
+#[instrument(level = "debug", skip(socket))]
+async fn websocket_connection(socket: WebSocket, addr: SocketAddr) {
+    let identifier = Uuid::new_v4();
+    debug!("WS client connected with ID: {identifier}");
+
+    let mut vehicles_receiver = control::subscribe_vehicles();
+
+    let (mut sender, mut receiver) = socket.split();
+
+    let send_task = tokio::spawn(async move {
+        while let Ok(vehicle) = vehicles_receiver.recv().await {
+            if sender
+                .send(ws::Message::Text(
+                    serde_json::to_string_pretty(&vehicle).unwrap().into(),
+                ))
+                .await
+                .is_err()
+            {
+                break;
+            }
+        }
+    });
+
+    // Handle incoming messages
+    while let Some(Ok(message)) = receiver.next().await {
+        match message {
+            ws::Message::Text(text) => {
+                trace!("WS client received from {identifier}: {text}");
+            }
+            ws::Message::Close(frame) => {
+                debug!("WS client {identifier} disconnected: {frame:?}");
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    // We should be disconnected now, let's remove it
+    debug!("WS client {identifier} removed");
+    if let Err(error) = send_task.await {
+        warn!("Sender task finished with error: {error:?}");
+    }
+}
