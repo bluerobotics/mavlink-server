eb433048e88c68d7c7d2efc0885a2784de0b4cec
Patrick José Pereira <patrickelectric@gmail.com>
2025-03-12 14:41:52 -0300

    drivers: rest: autopilot: Add paramater abstraction
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/lib/drivers/rest/autopilot/mod.rs
+++ src/lib/drivers/rest/autopilot/mod.rs
@@ -8,6 +8,88 @@ pub mod ardusub;
 pub mod parameters;
 
 #[derive(Debug, Clone, Copy, Deserialize, Serialize, PartialEq, Eq)]
+pub enum ParameterType {
+    /// 8-bit unsigned integer
+    Uint8,
+    /// 8-bit signed integer
+    Int8,
+    /// 16-bit unsigned integer
+    Uint16,
+    /// 16-bit signed integer
+    Int16,
+    /// 32-bit unsigned integer
+    Uint32,
+    /// 32-bit signed integer
+    Int32,
+    /// 64-bit unsigned integer
+    Uint64,
+    /// 64-bit signed integer
+    Int64,
+    /// 32-bit floating-point
+    Real32,
+    /// 64-bit floating-point
+    Real64,
+    /// Unknown
+    Unknown(u8),
+}
+
+impl ParameterType {
+    pub fn from_u8(value: u8) -> Self {
+        match value {
+            1 => Self::Uint8,
+            2 => Self::Int8,
+            3 => Self::Uint16,
+            4 => Self::Int16,
+            5 => Self::Uint32,
+            6 => Self::Int32,
+            7 => Self::Uint64,
+            8 => Self::Int64,
+            9 => Self::Real32,
+            10 => Self::Real64,
+            _ => Self::Unknown(value),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
+pub struct Parameter {
+    pub name: String,
+    pub value: f64,
+    pub param_type: ParameterType,
+    pub count: u16,
+    pub index: u16,
+}
+
+impl Parameter {
+    /// Onboard parameter id, terminated by NULL if the length is less than 16 human-readable chars and WITHOUT null termination (NULL) byte
+    /// if the length is exactly 16 chars - applications have to provide 16+1 bytes storage if the ID is stored as string
+    fn id(&self) -> [u8; 16] {
+        let mut buffer = [0u8; 16];
+        let bytes = self.name.as_bytes();
+        let len = bytes.len().min(16);
+        buffer[..len].copy_from_slice(&bytes[..len]);
+        buffer
+    }
+
+    pub fn string_from_param_id(param_id: &[u8; 16]) -> String {
+        String::from_utf8_lossy(param_id)
+            .to_string()
+            .trim_end_matches(|c| c == '\0')
+            .to_string()
+    }
+
+    pub fn from_param_value(param_value: mavlink::ardupilotmega::PARAM_VALUE_DATA) -> Self {
+        Self {
+            name: Self::string_from_param_id(&param_value.param_id),
+            value: param_value.param_value as f64,
+            param_type: ParameterType::from_u8(param_value.param_type as u8),
+            count: param_value.param_count,
+            index: param_value.param_index,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Deserialize, Serialize, PartialEq, Eq)]
 #[repr(u8)]
 pub enum AutoPilotType {
     /// Generic autopilot, full support for everything
