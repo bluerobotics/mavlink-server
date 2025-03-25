2277dd54f8315de45bb4b7ac7dd71f321c3466bc
Patrick José Pereira <patrickelectric@gmail.com>
2025-03-25 09:29:09 -0300

    webpage: app: Use new MessageInspectorWidget
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/webpage/src/app.rs
+++ src/webpage/src/app.rs
@@ -1,10 +1,10 @@
 use std::{collections::BTreeMap, sync::Arc};
 
 use chrono::prelude::*;
-use eframe::egui::{CollapsingHeader, Context};
+use eframe::egui::Context;
 use egui::mutex::Mutex;
 use egui_dock::{DockArea, DockState, NodeIndex, Style, TabViewer};
-use egui_extras::{Column, TableBody, TableBuilder};
+use egui_extras::TableBody;
 use egui_plot::{Line, Plot, PlotPoints};
 use ewebsock::{connect, WsReceiver, WsSender};
 use humantime::format_duration;
@@ -13,7 +13,6 @@ use url::Url;
 use web_sys::window;
 
 use crate::{
-    components_names,
     messages::{FieldInfo, MessageInfo, VehiclesMessages},
     stats::{
         drivers_stats::{DriversStatsHistorical, DriversStatsSample},
@@ -24,6 +23,7 @@ use crate::{
     tabs::{control::ControlTab, helper::HelperTab},
     widgets::driver_stats::DriverStatsWidget,
     widgets::hub_stats::HubStatsWidget,
+    widgets::message_inspector::MessageInspectorWidget,
     widgets::message_stats::MessageStatsWidget,
 };
 
@@ -562,120 +562,14 @@ impl App {
     }
 
     fn create_messages_ui(&self, ui: &mut eframe::egui::Ui) {
-        let search_query = self.search_query.to_lowercase();
-        eframe::egui::ScrollArea::vertical().show(ui, |ui| {
-            for (system_id, components) in &self.vehicles_mavlink {
-                let mut matching_components = BTreeMap::new();
-
-                for (component_id, messages) in components {
-                    let mut matching_messages = BTreeMap::new();
-
-                    for (message_id, message) in messages {
-                        let name = &message.name;
-                        let name_lower = name.to_lowercase();
-                        let message_matches = search_query.is_empty()
-                            || name_lower.contains(&search_query)
-                            || message_id.to_string().contains(&search_query);
-
-                        let mut matching_fields = BTreeMap::new();
-
-                        if message_matches {
-                            matching_fields
-                                .extend(message.fields.iter().map(|(k, v)| (k.clone(), v.clone())));
-                        } else {
-                            for (field_name, field_info) in &message.fields {
-                                let field_name_lower = field_name.to_lowercase();
-                                if field_name_lower.contains(&search_query) {
-                                    matching_fields.insert(field_name.clone(), field_info.clone());
-                                }
-                            }
-                        }
-
-                        if message_matches || !matching_fields.is_empty() {
-                            matching_messages.insert(
-                                message_id,
-                                (message.clone(), matching_fields, message_matches),
-                            );
-                        }
-                    }
-
-                    if !matching_messages.is_empty() {
-                        matching_components.insert(*component_id, matching_messages);
-                    }
-                }
-
-                if !matching_components.is_empty() {
-                    let _ =
-                        CollapsingHeader::new(format!("Vehicle ID: {system_id}"))
-                            .id_salt(ui.make_persistent_id(format!("vehicle_{system_id}")))
-                            .default_open(true)
-                            .show(ui, |ui| {
-                                for (component_id, messages) in matching_components {
-                                    let component_name = components_names::get_component_name(component_id);
-                                    let _ = CollapsingHeader::new(format!(
-                                        "Component ID: {component_id} ({component_name})"
-                                    ))
-                                    .id_salt(ui.make_persistent_id(format!(
-                                        "component_{system_id}_{component_id}"
-                                    )))
-                                    .default_open(true)
-                                    .show(ui, |ui| {
-                                        for (
-                                            message_id,
-                                            (message, matching_fields, _message_matches),
-                                        ) in messages
-                                        {
-                                            let name = message.name;
-                                            let mut message_header = CollapsingHeader::new(
-                                                format!("Message ID: {message_id}\t({name})",),
-                                            )
-                                            .default_open(true)
-                                            .id_salt(ui.make_persistent_id(format!(
-                                                "message_{system_id}_{component_id}_{message_id}"
-                                            )));
-
-                                            if self.expand_all {
-                                                message_header = message_header.open(Some(true));
-                                            } else if self.collapse_all {
-                                                message_header = message_header.open(Some(false));
-                                            } else if !search_query.is_empty() {
-                                                message_header = message_header.open(Some(true));
-                                            }
-
-                                            message_header.show(ui, |ui| {
-                                                let id_salt = ui.make_persistent_id(format!(
-                                                    "messages_table_{system_id}_{component_id}_{message_id}"
-                                                ));
-                                                TableBuilder::new(ui)
-                                                    .id_salt(id_salt)
-                                                    .striped(true)
-                                                    .column(Column::auto().at_least(100.))
-                                                    .column(Column::remainder().at_least(150.))
-                                                    .body(|mut body| {
-                                                        for (field_name, field_info) in
-                                                            &matching_fields
-                                                        {
-                                                            add_row_with_graph(
-                                                                &mut body, field_info, field_name,
-                                                            );
-                                                        }
-
-                                                        add_last_update_row(
-                                                            &mut body,
-                                                            self.now,
-                                                            message
-                                                                .last_sample_time
-                                                                .timestamp_micros(),
-                                                        );
-                                                    });
-                                            });
-                                        }
-                                    });
-                                }
-                            });
-                }
-            }
-        });
+        MessageInspectorWidget::new(
+            self.now,
+            &self.vehicles_mavlink,
+            &self.search_query,
+            self.collapse_all,
+            self.expand_all,
+        )
+        .show(ui);
     }
 
     fn create_hub_messages_stats_ui(&self, ui: &mut eframe::egui::Ui) {
