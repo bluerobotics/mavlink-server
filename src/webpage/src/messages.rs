8a0b6779a321bc976ba65afdccd068034c8fbb26
Patrick José Pereira <patrickelectric@gmail.com>
2025-04-05 16:29:46 -0300

    webpage: messages: Add FieldValue
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/webpage/src/messages.rs
+++ src/webpage/src/messages.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 
 use chrono::{DateTime, Utc};
-use ringbuffer::ConstGenericRingBuffer;
+use ringbuffer::{ConstGenericRingBuffer, RingBuffer};
 
 const BUFFERS_CAPACITY: usize = 128;
 
@@ -15,14 +15,33 @@ pub type MessageID = u32;
 pub struct MessageInfo {
     pub name: String,
     pub last_sample_time: DateTime<Utc>,
-    pub fields: BTreeMap<String, FieldInfo<f64>>,
+    pub fields: BTreeMap<String, FieldValue>,
+}
+
+#[derive(Clone, Debug)]
+pub enum FieldValue {
+    Numeric(FieldInfo<f64>),
+    Text(FieldInfo<String>),
 }
 
 #[derive(Clone, Default, Debug)]
 pub struct FieldInfo<T>
 where
-    f64: std::convert::From<T>,
-    T: Copy + std::fmt::Debug,
+    T: Clone + std::fmt::Debug,
 {
     pub history: ConstGenericRingBuffer<(DateTime<Utc>, T), BUFFERS_CAPACITY>,
+}
+
+impl<T> FieldInfo<T>
+where
+    T: Copy + std::fmt::Debug,
+    f64: From<T>,
+{
+    pub fn to_f64(&self) -> FieldInfo<f64> {
+        let mut new_field_info = FieldInfo::default();
+        for (time, value) in self.history.iter() {
+            new_field_info.history.push((*time, f64::from(*value)));
+        }
+        new_field_info
+    }
 }
