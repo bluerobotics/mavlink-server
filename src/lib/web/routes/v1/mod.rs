18bb0b7cd6db0300100a6bfb047e00fe28827cbd
Patrick José Pereira <patrickelectric@gmail.com>
2026-04-10 09:25:02 -0300

    lib: web: routes: mod: Add mavftp
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/lib/web/routes/v1/mod.rs
+++ src/lib/web/routes/v1/mod.rs
@@ -4,6 +4,7 @@ use tracing::*;
 
 pub mod info;
 pub mod log;
+pub mod mavftp;
 pub mod rest;
 pub mod stats;
 
@@ -11,6 +12,7 @@ pub mod stats;
 pub fn router() -> Router {
     Router::new()
         .nest("/rest", rest::router())
+        .nest("/mavftp", mavftp::router())
         .nest("/stats", stats::router())
         .nest("/log", log::router())
         .nest("/info", info::router())
