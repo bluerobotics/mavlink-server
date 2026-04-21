3305725787007eab0e6e820b7f9c72553e3431b8
Patrick José Pereira <patrickelectric@gmail.com>
2026-04-21 15:29:46 -0300

    lib: web: routes: mod: Add Vehicle Logs
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/lib/web/routes/v1/mod.rs
+++ src/lib/web/routes/v1/mod.rs
@@ -7,12 +7,14 @@ pub mod log;
 pub mod mavftp;
 pub mod rest;
 pub mod stats;
+pub mod vehicle_logs;
 
 #[instrument(level = "trace")]
 pub fn router() -> Router {
     Router::new()
         .nest("/rest", rest::router())
         .nest("/mavftp", mavftp::router())
+        .nest("/vehicle_logs", vehicle_logs::router())
         .nest("/stats", stats::router())
         .nest("/log", log::router())
         .nest("/info", info::router())
